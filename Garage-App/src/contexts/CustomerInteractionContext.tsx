7710f45d6b4c71663cb0523edbaa1a6d1daee7a8
Emmanuel <emmanuelnv01@gmail.com>
2025-11-28 16:52:45 +0000

    feat: create CustomerInteractionContext component

---
+++ Garage-App/src/contexts/CustomerInteractionContext.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type CustomerInteractionContextProps = {};
+
+interface CustomerInteractionContextType {}
+
+const CustomerInteractionContext = ({}: CustomerInteractionContextProps) => {
+  return <div></div>;
+};
+
+export default CustomerInteractionContext;
