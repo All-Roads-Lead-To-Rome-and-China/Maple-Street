bbe53fda669792b4a4c4c898cc69da6fe465f919
thushan204 <thushan.20@outlook.com>
2025-12-01 21:52:59 +0000

    updated routing for web-app navigation

--- Garage-App/src/App.tsx
+++ Garage-App/src/App.tsx
@@ -1,21 +1,23 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-} from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
+import HomePage from "./pages/Homepage"
 import CustomerPortal from "./pages/CustomerPortal";
 import CustomerRegistrationForm from "./components/CustomerRegistrationForm";
 import CustomerProfile from "./components/CustomerProfile";
 import CustomerList from "./components/CustomerList";
 import BookingForm from "./components/BookingForm";
 import BookingSuccess from "./components/BookingSuccess";
+import CustomerBookings from "./components/CustomerBookings";
 
 // Wrapper to extract customerId from URL and pass to BookingForm
 const BookingFormWrapper = () => {
   const { customerId } = useParams();
   return <BookingForm customerId={customerId!} />;
+};
+
+// Wrapper to extract customerId from URL and pass to CustomerBookings
+const CustomerBookingsWrapper = () => {
+  const { customerId } = useParams();
+  return <CustomerBookings key={customerId} />;
 };
 
 function App() {
@@ -27,15 +29,10 @@ function App() {
 
         {/* Customer Journey */}
         <Route path="/customer" element={<CustomerPortal />} />
-        <Route
-          path="/customer/register"
-          element={<CustomerRegistrationForm />}
-        />
+        <Route path="/customer/register" element={<CustomerRegistrationForm />} />
         <Route path="/customer/profile" element={<CustomerProfile />} />
-        <Route
-          path="/customer/:customerId/booking"
-          element={<BookingFormWrapper />}
-        />
+        <Route path="/customer/:customerId/booking" element={<BookingFormWrapper />} />
+        <Route path="/customer/:customerId/bookings" element={<CustomerBookingsWrapper />} />
 
         {/* Booking Success */}
         <Route path="/booking-success" element={<BookingSuccess />} />
