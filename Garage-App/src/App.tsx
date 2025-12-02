ed4607619675a9805e475cdbde0723f08c3c3019
thushan204 <thushan.20@outlook.com>
2025-12-02 22:05:47 +0000

    updated navigation

--- Garage-App/src/App.tsx
+++ Garage-App/src/App.tsx
@@ -4,7 +4,7 @@ import {
   Route,
   useParams,
 } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage"
 import CustomerPortal from "./pages/CustomerPortal";
 import CustomerRegistrationForm from "./components/CustomerRegistrationForm";
 import CustomerProfile from "./components/CustomerProfile";
@@ -14,6 +14,8 @@ import BookingSuccess from "./components/BookingSuccess";
 import CustomerBookings from "./components/CustomerBookings";
 import StaffDashboard from "./pages/StaffDashboard";
 import InventoryList from "./components/InventoryList";
+import AppointmentScheduling from "./components/AppointmentScheduling";
+import StaffScheduling from "./components/StaffScheduling";
 
 // Wrapper to extract customerId from URL and pass to BookingForm
 const BookingFormWrapper = () => {
@@ -56,22 +58,13 @@ function App() {
         {/* Staff Journey */}
         <Route path="/staff" element={<StaffDashboard />} />
         <Route path="/staff/customers" element={<CustomerList />} />
-        <Route
-          path="/staff/appointments"
-          element={<div>Appointment Scheduling Component</div>}
-        />
-        <Route
-          path="/staff/inventory"
-          element={<InventoryList/>}
-        />
+        <Route path="/staff/appointments" element={<AppointmentScheduling />} />
+        <Route path="/staff/inventory" element={<InventoryList />} />
         <Route
           path="/staff/invoices"
           element={<div>Invoices & Payments Component</div>}
         />
-        <Route
-          path="/staff/scheduling"
-          element={<div>Staff Scheduling Component</div>}
-        />
+        <Route path="/staff/scheduling" element={<StaffScheduling />} />
         <Route
           path="/staff/compliance"
           element={<div>Compliance & Safety Component</div>}
